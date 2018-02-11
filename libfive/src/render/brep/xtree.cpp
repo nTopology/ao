2c8f16798bc2a6b1103e72c4ad35da1fc8eaf155
Bradley Rothenberg <brad@ntopology.com>
2018-02-11 13:21:59 -0500

    remove fstream

--- libfive/src/render/brep/xtree.cpp
+++ libfive/src/render/brep/xtree.cpp
@@ -20,7 +20,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <numeric>
 #include <functional>
 #include <limits>
-#include <fstream>
 
 #include <cmath>
 
